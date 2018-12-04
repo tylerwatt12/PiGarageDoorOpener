08801533f2200d95a7903e21cd7ef8e91b1b1df9
tylerwatt12 <tylerwatt12@gmail.com>
2018-12-03 22:52:41 -0500

    bug fix, removed extra code

--- index.php
+++ index.php
@@ -4,15 +4,24 @@
 #error_reporting(E_ALL);
 
 /*    CONFIG    */
-$relayPinMap[1] = array('relayPin' => '5', 'LEDPin' => '24', 'name' => 'One Car Door'); // set relay pin and LED pin for garage opener 1
-$relayPinMap[2] = array('relayPin' => '4', 'LEDPin' => '25'. 'name' => 'Two Car Door'); // set relay pin and LED pin for garage opener 2
-$message = " was triggered";
+##### Name of JSON IoT Device
 $thingName = 'Garage Door Opener';
+
+##### Configuration for multiple garage doors, mapping garage door number to GPIO pins for relay and LED
+$relayPinMap[1] = array('relayPin' => '5', 'LEDPin' => '24'); // set relay pin and LED pin for garage opener 1
+$relayPinMap[2] = array('relayPin' => '4', 'LEDPin' => '25'); // set relay pin and LED pin for garage opener 2
+
+$relayHoldTime = 1000; // ms to hold the relay when triggered
+
+##### LED flashing indicator configuration
+$LEDspeed = 10; // how many ms between light turns on/off
+$LEDlength = 5; // how many times the light turns on/off before flash sequence is complete
 /*  END CONFIG  */
 
-foreach ($relayPinMap as $relay => $relayArray) { // initialize and set pins to output
-	wiringpi("mode",$relayArray['relayPin'],"out");
-	wiringpi("mode",$relayArray['LEDPin'],"out");
+foreach ($relayPinMap as $relay => $relayArray) { // initialize gpio pins
+	wiringpi("mode",$relayArray['relayPin'],"out"); // set output for relay
+	wiringpi("mode",$relayArray['LEDPin'],"out"); // set output for LED
+	wiringpi("write",$relayArray['LEDPin'],0); // turn on LED
 }
 
 // standard function for all iot devices
@@ -55,30 +64,33 @@ function flashyflash($led,$speed = 10, $length = 5){ // flashes the LED indicato
 	// length - how many intervals before termination;
 	wiringpi("write",$led,0); // turn on LEDs
 	while (@$i <= $length) { // flash for $length times
-		wiringpi("write",$led,1);
+		wiringpi("write",$led,1); // led off
 		usleep($speed*1000);
-		wiringpi("write",$led,0);
+		wiringpi("write",$led,0); // led on
 		usleep($speed*1000);
 		@$i++;
 	}
 }
 
 function trigger($pin,$ms,$led){ // simulates a button press
-	flashyflash($led);
+	flashyflash($led,$LEDspeed, $LEDlength); // flash LEDs before triggering relay
 	if (@!$_GET['debug']) {
-		wiringpi("write",$pin,1);
-		usleep($ms*1000);
-		wiringpi("write",$pin,0);
+		wiringpi("write",$pin,1); // press relay
+		usleep($ms*1000); // wait specified amount of time 
+		wiringpi("write",$pin,0); // release relay
 	}
 }
-if ($_GET['trigger']) {
+
+///////// EXECUTED AT RUNTIME
+if ($relayPinMap[$_GET['trigger'] ]['relayPin']) {
 	trigger($relayPinMap[ $_GET['trigger'] ]['relayPin'],
-						1000,
-						$relayPinMap[ $_GET['trigger'] ]['LEDPin']); // hold relay for 1 second
+						$relayHoldTime,
+						$relayPinMap[ $_GET['trigger'] ]['LEDPin']); // hold relay
 	$response = TRUE;
 }
 
 $runmode = $_GET['debug'] ? "Debug" : "Production";
+
 $addtlInfo = $_GET['addtlInfo'] ? addtlInfo():NULL;
 
 $output = array('thingName' => $thingName, 'runmode' => $runmode, 'response' => $response , 'addtlInfo' => $addtlInfo);
